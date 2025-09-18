05c6035155cf86504c73b2e5b6d8d65b81cb0e55
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2025-09-18 15:09:03 +0300

    create environment

---
+++ flutter_redesign/README.md
@@ -0,0 +1,3 @@
+# flutter_redesign
+
+A new Flutter project.
---
+++ flutter_redesign/android/app/src/main/kotlin/com/example/flutter_redesign/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_redesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
